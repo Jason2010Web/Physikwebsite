52f4098a97aeb19006e739a09b2cc841c259fd1b
Jason2010Web <163747503+Jason2010Web@users.noreply.github.com>
2024-05-12 15:32:52 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,22 +1,22 @@
-let passwords = [
-    Mama = {
-        password: "mnspoens*WÜa!j972)jndv",
-        name: "Mama"
-    },
-    Papa = {
-        password: "kjdszsvk*HFä!j893)hjds",
-        name: "Papa"
-    },
-    Ich = {
-        password: "admin@Jason.Web!",
-        name: "Jason"
-    }
-]
-function checkPassword(){
-for (let i = 0; i < passwords.length; i++){
-    if (document.querySelector(".passwordfield").value == passwords[i].password){
-        document.querySelector(".Codefenster").style.display = "none"
-        document.querySelector(".Webseitencontent").style.display = "block"
-    }
-}
-}
+// let passwords = [
+//     Mama = {
+//         password: "mnspoens*WÜa!j972)jndv",
+//         name: "Mama"
+//     },
+//     Papa = {
+//         password: "kjdszsvk*HFä!j893)hjds",
+//         name: "Papa"
+//     },
+//     Ich = {
+//         password: "admin@Jason.Web!",
+//         name: "Jason"
+//     }
+// ]
+// function checkPassword(){
+// for (let i = 0; i < passwords.length; i++){
+//     if (document.querySelector(".passwordfield").value == passwords[i].password){
+//         document.querySelector(".Codefenster").style.display = "none"
+//         document.querySelector(".Webseitencontent").style.display = "block"
+//     }
+// }
+// }
